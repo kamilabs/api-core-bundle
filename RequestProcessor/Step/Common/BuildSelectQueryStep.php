5e10bb3dca30b1770ac1246e7211467c5c22210c
Iakov Mishchenko <iakov@kamilabs.com>
2018-05-13 14:35:47 +0300

    Added groupBy

--- RequestProcessor/Step/Common/BuildSelectQueryStep.php
+++ RequestProcessor/Step/Common/BuildSelectQueryStep.php
@@ -52,6 +52,7 @@ class BuildSelectQueryStep extends AbstractStep
             $this->addSelectIfEligible($property, $queryBuilder);
         }
         $queryBuilder->addSelect(sprintf('partial e.{id, %s}', implode(', ', $this->accessible)));
+        $queryBuilder->groupBy('e.id');
 
         return $this->createResponse(['query_builder' => $queryBuilder]);
     }
